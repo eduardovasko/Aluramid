e72650af911226056830046da2b404e0fd3afb91
eduardovasko <130376753+eduardovasko@users.noreply.github.com>
2023-11-06 10:09:42 -0300

    .

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -5,9 +5,19 @@ function tocaSomPom () {
 const listadeTeclas = document.querySelectorAll('.tecla');
 let contator = 0;
 
-while(contador<9){
-listadeTeclas[contador].onclick = tocaSomPom;
-
+for (let contador = 0; contador < listaDeTeclas.length; contador++){ 
+    const tecla = listadeTeclas[Contador] 
+    const instrumento = tecla.classlist[1];
+    const idaudio = `#som_ ${instrumento}`;
+       console.log(instrumento); 
+       tecla.onclick = function (){
+         tocaSom(idaudio) 
+       }
 contador = contador + 1;
-
+1 = function(evento){
+    if(evento.code == 'enter')
+listadeTeclas.classlit.add('ativa')
 }
+tecla.onKeyup = function(){
+    tecla.classlist.remove('ativa')}
+
